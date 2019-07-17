2a1c2f921ec180426a05ee2d0dc7a0f2d6e03da1
Marcus Pope <atmos@marcuspope.com>
2019-07-17 08:38:33 -0600

    feat: Added reporting feature to pass client-side mocha test results back to server.

--- test/web-bootstrap.js
+++ test/web-bootstrap.js
@@ -10,3 +10,22 @@ global.workingSourceMaps = false;
 // @NOTE: Use webpack to require all tests
 var context = require.context('../src', true, /.+-(test|test-web)\.js$/);
 context.keys().forEach(context);
+
+after(() => {
+  // Post the results back to web-dev-server for backend reporting
+  fetch('/results', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      pass: find('li.passes em').innerText,
+      fail: find('li.failures em').innerText,
+      dur: find('li.duration em').innerText
+    })
+  })
+    .then(res => res.text())
+    .then(res => {
+      if (res === 'close') {
+        open(location, '_self').close();
+      }
+    });
+});
--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const baseConfig = require('../webpack.config.js');
+const bodyParser = require('body-parser');
 
 const testConfig = Object.assign({}, baseConfig, {
   entry: path.join(__dirname, 'web-bootstrap.js'),
@@ -10,7 +11,20 @@ const testConfig = Object.assign({}, baseConfig, {
     new HtmlWebpackPlugin({
       template: './test/mocha-test-template.html'
     })
-  ]
+  ],
+  devServer: {
+    // This allows the mocha test suite to report back on the success of the tests
+    before: app => {
+      app.post('/results', bodyParser.json(), function(req, res) {
+        // Passing = 100% passing in under 2 seconds
+        const pass = req.body.fail === '0' && req.body.dur * 1 < 2;
+        // Based on the results of the test, exit with 0 or 1
+        process.exit(pass ? 0 : 1);
+        // Tell the browser to close or remain open for debugging
+        res.send(pass ? 'close' : 'hold');
+      });
+    }
+  }
 });
 
 testConfig.module.rules.push(
